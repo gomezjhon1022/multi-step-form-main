302efa7d0c4b80e8bc4ddfcc6a9348a3c86a955d
Jhon Gomez <jhonjairogomez92@gmail.com>
2023-08-03 09:07:03 +0200

    Add step 5

--- src/components/ThankYou/index.js
+++ src/components/ThankYou/index.js
@@ -1,5 +1,24 @@
+import './ThankYou.css';
+import  imagen  from '../../assets/icon-thank-you.svg';
 function ThankYou () {
-  return(<div>Than you</div>)
+
+  return(
+    <div className="card__bg">
+    <div className='card__thankyou'>
+      <img className='thankyou__image' src={imagen} alt='Thank you!'
+      ></img>
+      <h2 className='card__title'>Thank you!</h2>
+      <p className='card__description'>Thanks for confirm your subscription!<br/>
+        We hope you have fun using our<br/>
+        platform. If you ever need suport,<br/>
+        please feel free to email us at<br/>
+        support@loremgaming.com.
+      </p>
+
+    </div>
+  </div>
+
+  )
 }
 
 export { ThankYou };
